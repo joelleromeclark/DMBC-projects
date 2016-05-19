c5b92121963037118bd4bd1732bad5148ced87c1
Joelle Romero-Clark <joelle.romero@yahoo.com>
2016-05-19 15:10:41 -0600

    first commit

---
+++ js/rollovers.js
@@ -0,0 +1,34 @@
+//Rollover 1
+
+var rollon1 = new Image();
+rollon1.src = "img/deer-on.jpg";
+
+var rollout1 = new Image();
+rollout1.src = "img/deer.jpg";
+
+function on1(){
+	document.getElementById("rollover1").src = rollon1.src;
+}
+function out1(){
+	document.getElementById("rollover1").src = rollout1.src;
+}
+
+//End of Rollover 1
+
+//Rollover 2
+
+var rollon2 = new Image();
+rollon2.src = "img/nail.jpg";
+
+var rollout2 = new Image();
+rollout2.src = "img/balloon.jpg";
+
+function on2(){
+	document.getElementById("rollover2").src = rollon2.src;
+}
+
+function out2(){
+	document.getElementById("rollover2").src = rollout2.src;
+}
+
+//End of Rollover 2
